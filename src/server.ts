a3680f099b655cd1e22332734d6d8b4344332347
dirkzukunft <git@qf3.de>
2021-09-08 10:40:12 +0200

    Fix express port for Heroku

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
-const port = process.env.EXPRESS_PORT || 3001;
+const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
