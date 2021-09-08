7bef8c9cad29a4574e3fcca9ba164b026c4de1cd
dirkzukunft <git@qf3.de>
2021-09-08 12:40:40 +0200

    Add story

---
+++ src/app/components/Headline/Headline.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, Story } from '@storybook/react';
+import React from 'react';
+import type { HeadlineProps } from './Headline';
+import Headline from './Headline';
+
+export default {
+  title: 'Components',
+  component: Headline,
+  argTypes: {
+    level: {
+      options: [1, 2, 3, 4, 5, 6],
+      control: { type: 'select' },
+    },
+    styling: {
+      options: [`default`, `large`, `logo`],
+      control: { type: 'select' },
+    },
+    children: { control: { type: 'text' } },
+  },
+} as Meta;
+
+const Template: Story<HeadlineProps> = (args) => <Headline {...args}></Headline>;
+
+export const headline = Template.bind({});
+headline.args = {
+  level: 2,
+  styling: `default`,
+  children: `This is my headline`,
+};
