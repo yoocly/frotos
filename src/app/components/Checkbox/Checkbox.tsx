a8f2906dd924419988a3346682194af4305b8943
dirkzukunft <git@qf3.de>
2021-09-11 00:07:14 +0200

    Add checkbox component

---
+++ src/app/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Checkbox.module.css';
+
+export type CheckboxProps = {
+  checked?: boolean;
+  onChange: () => void;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Checkbox({
+  checked = false,
+  onChange,
+  children,
+  className = '',
+}: CheckboxProps): JSX.Element {
+  return (
+    <label className={`${styles.checkbox} ${className}`}>
+      <input type="checkbox" checked={checked} onChange={onChange} />
+      <span>{children}</span>
+    </label>
+  );
+}
