ff0991ac88c2190dbc3a365e587dafdac083b1bc
dirkzukunft <87307560+dirkzukunft@users.noreply.github.com>
2021-09-14 14:17:33 +0200

    Add component Button (#22)

---
+++ src/app/components/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { COLORS } from '../../lib/colors';
+import { ICONS } from '../Icon/icons';
+import type { ButtonProps } from './Button';
+import Button from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    icon: {
+      options: Object.keys(ICONS),
+      control: { type: 'select' },
+    },
+    color: {
+      options: Object.keys(COLORS),
+      control: { type: 'select' },
+    },
+  },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args}></Button>;
+
+export const button = Template.bind({});
+button.args = {
+  icon: 'search',
+  text: 'Search',
+  small: false,
+  transparent: false,
+  onClick: () => console.log('Clicked'),
+};
---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import type { colorKey } from '../../lib/colors';
+import { COLORS } from '../../lib/colors';
+import Icon from '../Icon/Icon';
+import type { iconKey } from '../Icon/icons';
+import styles from './Button.module.css';
+
+export type ButtonProps = {
+  icon?: iconKey;
+  text?: string;
+  color?: colorKey;
+  small: boolean;
+  transparent: boolean;
+  onClick: () => void;
+  className?: string;
+};
+
+export default function Button({
+  icon = 'none',
+  text,
+  color = 'lightPrimary',
+  small = false,
+  transparent = false,
+  onClick,
+  className = '',
+}: ButtonProps): JSX.Element {
+  const { colorClass } = COLORS[color];
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.button} ${colorClass} 
+        ${small && styles.small} ${transparent && styles.transparent} ${className}`}
+    >
+      {icon && (
+        <Icon icon={icon} color={color} width={small ? '1rem' : ''} height={small ? '1rem' : ''} />
+      )}
+      {text && <div className={`${colorClass}`}>{text}</div>}
+    </button>
+  );
+}
