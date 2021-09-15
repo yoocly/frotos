504f9c0d2c3d58759fcb177e3cacfd0dc140bc98
dirkzukunft <git@qf3.de>
2021-09-15 16:54:11 +0200

    Add BackgroundImage component

---
+++ src/app/components/BackgroundImage/BackgroundImage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './BackgroundImage.module.css';
+
+export type BackgroundImageProps = {
+  image?: string;
+  dim?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+};
+
+export default function BackgroundImage({
+  image = 'splash',
+  dim = true,
+  children,
+  className = '',
+}: BackgroundImageProps): JSX.Element {
+  return (
+    <div
+      style={{ backgroundImage: `url("src/assets/${image}.webp")` }}
+      className={`${styles.backgroundImage} ${className}`}
+    >
+      {dim ? <div className={styles.dim}>{children}</div> : children}
+    </div>
+  );
+}
