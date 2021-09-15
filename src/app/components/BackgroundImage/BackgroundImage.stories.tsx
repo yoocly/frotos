edc12ae9f0b71b158b83571eea3e40419bb346b8
dirkzukunft <git@qf3.de>
2021-09-15 16:54:19 +0200

    Add BackgroundImage story

---
+++ src/app/components/BackgroundImage/BackgroundImage.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, Story } from '@storybook/react';
+import React from 'react';
+import type { BackgroundImageProps } from './BackgroundImage';
+import BackgroundImage from './BackgroundImage';
+
+export default {
+  title: 'Components/Background Image',
+  component: BackgroundImage,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<BackgroundImageProps> = (args) => (
+  <BackgroundImage {...args}></BackgroundImage>
+);
+
+export const backgroundImage = Template.bind({});
+backgroundImage.args = {
+  image: `splash`,
+  dim: true,
+  children: `This is my backgroundimage`,
+};
