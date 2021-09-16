b37bc148cfa288c1a3cdda3d803cc819b856c999
dirkzukunft <git@qf3.de>
2021-09-16 23:47:19 +0200

    Add Spinner and click handling to SearchResult

--- src/app/components/SearchResult/SearchResult.tsx
+++ src/app/components/SearchResult/SearchResult.tsx
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Masonry from 'react-masonry-component';
 import type { image } from '../../../api/apis';
 import SearchResultImage from '../SearchResultImage/SearchResultImage';
+import Spinner from '../Spinner/Spinner';
 import styles from './SearchResult.module.css';
 
 export type SearchResultProps = {
   images: image[];
-  onImageClick: () => void;
+  onImageClick: (id: string) => void;
+  onCollectionClick: (id: string) => void;
   className?: string;
 };
 
-export default function SearchResult({ images, className = '' }: SearchResultProps): JSX.Element {
-  const columns = Math.min(5, Math.floor(window.innerWidth / 150));
+export default function SearchResult({
+  images,
+  onImageClick,
+  onCollectionClick,
+  className = '',
+}: SearchResultProps): JSX.Element {
+  const [masonryComplete, setMasonryComplete] = useState<boolean>(false);
+
+  const columns = Math.min(4, Math.floor(window.innerWidth / 150));
   const resultWidth = 100 / columns;
 
   return (
-    <div className={`${styles.searchResult} ${className}`}>
-      <Masonry updateOnEachImageLoad={true} className={styles.searchGrid}>
+    <>
+      <Spinner show={!masonryComplete} className={styles.spinner} />
+      <Masonry
+        updateOnEachImageLoad={false}
+        elementType={'section'}
+        onLayoutComplete={() => setMasonryComplete(true)}
+        className={`${!masonryComplete ? styles.loading : styles.loaded} ${className}`}
+      >
         {images.map((image) => {
           return (
             <SearchResultImage
               image={image}
               width={`calc(${resultWidth}%`} // - 2 * ${columns} * var(--gapSmall))`}
               inCollection={false}
-              onClick={() => console.log(`clicked image ${image.id}`)}
-              onCollectionClick={() => console.log(`clicked collection on image ${image.id}`)}
+              onClick={() => onImageClick(image.id)}
+              onCollectionClick={() => onCollectionClick(image.id)}
               className={styles.searchResultImage}
               key={`${image.api}${image.id}`}
             />
           );
         })}
       </Masonry>
-    </div>
+    </>
   );
 }
