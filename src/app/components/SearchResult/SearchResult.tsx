df2198f63925290f47377cc3ddf6299008eb62b8
dirkzukunft <git@qf3.de>
2021-09-16 23:45:48 +0200

    Add Masonry to SearchResult component

--- src/app/components/SearchResult/SearchResult.tsx
+++ src/app/components/SearchResult/SearchResult.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Masonry from 'react-masonry-component';
 import type { image } from '../../../api/apis';
 import SearchResultImage from '../SearchResultImage/SearchResultImage';
 import styles from './SearchResult.module.css';
@@ -10,18 +11,26 @@ export type SearchResultProps = {
 };
 
 export default function SearchResult({ images, className = '' }: SearchResultProps): JSX.Element {
+  const columns = Math.min(5, Math.floor(window.innerWidth / 150));
+  const resultWidth = 100 / columns;
+
   return (
     <div className={`${styles.searchResult} ${className}`}>
-      {images.map((image) => {
-        return (
-          <SearchResultImage
-            image={image}
-            inCollection={false}
-            onClick={() => console.log(`clicked image ${image.id}`)}
-            onCollectionClick={() => console.log(`clicked collection on image ${image.id}`)}
-          />
-        );
-      })}
+      <Masonry updateOnEachImageLoad={true} className={styles.searchGrid}>
+        {images.map((image) => {
+          return (
+            <SearchResultImage
+              image={image}
+              width={`calc(${resultWidth}%`} // - 2 * ${columns} * var(--gapSmall))`}
+              inCollection={false}
+              onClick={() => console.log(`clicked image ${image.id}`)}
+              onCollectionClick={() => console.log(`clicked collection on image ${image.id}`)}
+              className={styles.searchResultImage}
+              key={`${image.api}${image.id}`}
+            />
+          );
+        })}
+      </Masonry>
     </div>
   );
 }
