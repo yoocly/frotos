b817ee2ec99068f2e3bdc2e44d6ba8c67a1ec29b
dirkzukunft <git@qf3.de>
2021-09-21 16:42:57 +0200

    Add Component Modal

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { isMobileOnly } from 'react-device-detect';
+import Button from '../Button/Button';
+import styles from './Modal.module.css';
+
+export const MODAL_POSITIONS = ['centered', 'bottomRight'] as const;
+export const modalDefaultSize = {
+  desktop: { height: '80vh', width: '80vh', maxHeight: '90vh', maxWidth: '90vh' },
+  mobile: { height: '100vh', width: '100vh', maxHeight: '', maxWidth: '' },
+};
+export type modalSize = typeof modalDefaultSize;
+
+export type ModalProps = {
+  show?: boolean;
+  backgroundOverlay?: boolean;
+  backgroundBlur?: boolean;
+  size?: modalSize;
+  width?: string;
+  position?: typeof MODAL_POSITIONS[number];
+  closeButton?: boolean;
+  backButton?: boolean;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Modal({
+  show = true,
+  backgroundOverlay = true,
+  backgroundBlur = false,
+  size = modalDefaultSize,
+  position = 'centered',
+  closeButton = false,
+  backButton = false,
+  children,
+  className = '',
+}: ModalProps): JSX.Element {
+  const [visible, setVisible] = useState<boolean>(show);
+
+  if (!visible) return <></>;
+
+  const sizes = isMobileOnly ? size.mobile : size.desktop;
+
+  const modal = (
+    <div style={sizes} className={`${styles.modal} ${styles[position]} ${className}`}>
+      {closeButton && (
+        <Button
+          icon="close"
+          color="light"
+          large
+          transparent
+          onClick={() => setVisible(!visible)}
+          className={styles.closeButton}
+        />
+      )}
+      {backButton && (
+        <Button
+          icon="back"
+          color="light"
+          large
+          transparent
+          onClick={() => setVisible(!visible)}
+          className={styles.backButton}
+        />
+      )}
+      <div className={styles.modalContent}>{children}</div>
+    </div>
+  );
+
+  const overlay = (
+    <div className={`${styles.overlay} ${backgroundBlur ? styles.backgroundBlur : ``}`}></div>
+  );
+
+  return (
+    <>
+      {backgroundOverlay && overlay} {modal}
+    </>
+  );
+}
