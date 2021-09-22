fbce62ff957912990a72c8f173e7882f9b85f60b
dirkzukunft <git@qf3.de>
2021-09-22 11:15:26 +0200

    Add story

---
+++ src/app/components/NavBarImage/NavBarImage.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, Story } from '@storybook/react';
+import React from 'react';
+import type { NavBarImageProps } from './NavBarImage';
+import { NAVBARIMAGE_ITEMS } from './NavBarImage';
+import NavBarImage from './NavBarImage';
+
+export default {
+  title: 'Components/Nav Bar Image',
+  component: NavBarImage,
+  argTypes: {
+    active: {
+      options: NAVBARIMAGE_ITEMS,
+      control: { type: 'select' },
+    },
+  },
+} as Meta;
+
+const Template: Story<NavBarImageProps> = (args) => <NavBarImage {...args}></NavBarImage>;
+
+export const navBarImage = Template.bind({});
+navBarImage.args = { onClick: (item) => console.log(`clicked ${item}`) };
