1e79cf248a2ac575a64e3e60cc42b8e7af39c7c5
dirkzukunft <git@qf3.de>
2021-09-24 11:50:08 +0200

    Use addUser model function

--- src/lib/routes/user.ts
+++ src/lib/routes/user.ts
@@ -1,20 +1,13 @@
 import { Router } from 'express';
-import { dbInsertOne } from '../../utils/database';
 import type { user } from '../models/user';
-import { hashUserPassword } from '../models/user';
+import { addUser } from '../models/user';
 
 const router = Router();
 
 router.post('/user/add', async (req, res) => {
   const user: user = req.body;
-  const userPasswordHashed = await hashUserPassword(user);
-
-  const assertNoClearPassword = (user: user) =>
-    user.password === undefined ? true : 'Payload contains clear password';
-
-  const { status, response } = await dbInsertOne(userPasswordHashed, assertNoClearPassword);
+  const { status, response } = await addUser(user);
   res.status(status).json({ response });
 });
 
-// const r = jwt.sign(userData.password, process.env.JWT_SECRET || '');
 export default router;
