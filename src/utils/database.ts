534a8f8575d0c353c6efc994a43d59023a2cfe6f
dirkzukunft <git@qf3.de>
2021-09-24 11:35:58 +0200

    Add generic InsertOne method

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,7 +1,16 @@
-import type { Collection } from 'mongodb';
+import type { Collection, InsertOneResult } from 'mongodb';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
+
+export type dbResult<dbActionType, payload> = Promise<{
+  status: number;
+  response: {
+    result?: dbActionType;
+    error?: unknown;
+    payload: payload;
+  };
+}>;
 
 let client: MongoClient;
 
@@ -17,4 +26,21 @@ export async function connectDb(): Promise<void> {
 
 export function getCollection<T>(collectionName: string): Collection<T> {
   return client.db().collection<T>(collectionName);
+}
+
+export async function dbInsertOne<T>(
+  payload: T,
+  assertionCallback: (payload: T) => string | boolean = () => true
+): dbResult<InsertOneResult, T> {
+  const assertionResult = assertionCallback(payload);
+  if (assertionResult !== true)
+    return { status: 500, response: { error: assertionResult, payload } };
+
+  try {
+    const result = await getCollection('users').insertOne(payload);
+    return { status: 201, response: { result, payload } };
+  } catch (error) {
+    console.log(payload);
+    return { status: 500, response: { error, payload } };
+  }
 }
