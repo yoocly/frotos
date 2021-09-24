968abef34a5cfad5e7cdfbf17baf31fde12ce8d5
dirkzukunft <git@qf3.de>
2021-09-24 12:28:16 +0200

    Add /user/check route and checkUserExists function

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -1,6 +1,6 @@
 import type { InsertOneResult } from 'mongodb';
 import type { dbResult } from '../../utils/database';
-import { dbInsertOne } from '../../utils/database';
+import { dbFindOne, dbInsertOne } from '../../utils/database';
 import { hashPassword } from '../../utils/hashPassword';
 
 export type user = { username: string; password?: string; passwordHash?: string };
@@ -8,6 +8,7 @@ export type user = { username: string; password?: string; passwordHash?: string 
 export async function hashUserPassword(user: user): Promise<user> {
   const hash = user.password !== undefined ? await hashPassword(user.password) : undefined;
   const userPasswordHashed = { ...user, passwordHash: hash, password: undefined };
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password, ...userWithoutPassword } = userPasswordHashed;
   return userWithoutPassword;
@@ -19,5 +20,14 @@ export async function addUser(user: user): dbResult<InsertOneResult, user> {
   const assertNoClearPassword = (user: user) =>
     user.password === undefined ? true : 'Payload contains clear password';
 
-  return await dbInsertOne(userPasswordHashed, assertNoClearPassword);
+  return await dbInsertOne('user', userPasswordHashed, assertNoClearPassword);
+}
+
+export async function checkUserExists(user: user): dbResult<unknown, user> {
+  const { username } = user;
+
+  const dbResult = await dbFindOne('users', { username });
+  dbResult.response.result = dbResult.status === 200;
+
+  return dbResult;
 }
--- src/lib/routes/user.ts
+++ src/lib/routes/user.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
-import type { user } from '../models/user';
-import { addUser } from '../models/user';
+import { addUser, checkUserExists } from '../models/user';
 
 const router = Router();
 
 router.post('/user/add', async (req, res) => {
-  const user: user = req.body;
-  const { status, response } = await addUser(user);
+  const { status, response } = await addUser(req.body);
   res.status(status).json({ response });
+});
+
+router.post('/user/check', async (req, res) => {
+  // const { status, response } =
+  res.json(await checkUserExists(req.body));
+  // res.status(status).json({ response });
 });
 
 export default router;
