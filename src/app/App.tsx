c89fe6961da2801f0116aa969a881a7f33fdc655
dirkzukunft <git@qf3.de>
2021-10-01 22:47:04 +0200

    Fix imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
