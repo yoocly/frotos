ed60d996abcfc25dda506fae96d93fe234719956
dirkzukunft <git@qf3.de>
2021-10-04 14:15:19 +0200

    Add ImagePalette component

---
+++ src/app/components/ImagePalette/ImagePalette.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import type { image } from '../../../lib/types/image';
+import { useImagePalette } from '../../hooks/useImagePalette';
+import Spinner from '../Spinner/Spinner';
+import styles from './ImagePalette.module.css';
+
+export type ImagePaletteProps = {
+  image: image | null;
+  className?: string;
+};
+
+export default function ImagePalette({ image, className = '' }: ImagePaletteProps): JSX.Element {
+  const palette = useImagePalette(image);
+
+  return (
+    <div className={`${styles.imagePalette} ${className}`}>
+      {palette ? (
+        <>
+          <div className={styles.title}>Image colors</div>
+          <div className={styles.title}>Complementary colors</div>
+          {palette.map((color) => {
+            const { css, complementCss: complementcss, lightTextColor: lighttextcolor } = color;
+            return (
+              <>
+                <div
+                  className={styles.colorBox}
+                  style={{
+                    background: css,
+                    color: lighttextcolor ? `var(--medium) ` : `var(--dark) `,
+                  }}
+                >
+                  {css}
+                </div>
+                <div
+                  className={styles.colorBox}
+                  style={{
+                    background: complementcss,
+                    color: lighttextcolor ? `var(--medium) ` : `var(--dark) `,
+                  }}
+                >
+                  {complementcss}
+                </div>
+              </>
+            );
+          })}
+        </>
+      ) : (
+        <Spinner className={styles.spinner} />
+      )}
+    </div>
+  );
+}
