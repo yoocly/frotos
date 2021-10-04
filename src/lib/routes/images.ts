98866130dfed69cd8802a548fdae74e50c9c67b5
dirkzukunft <87307560+dirkzukunft@users.noreply.github.com>
2021-10-04 13:27:00 +0200

    Extract colors and palettes of an image (#76)

--- src/lib/routes/images.ts
+++ src/lib/routes/images.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { addImage, deleteImage, downloadImage, getImage, images } from '../models/images';
+import {
+  addImage,
+  deleteImage,
+  downloadImage,
+  getImage,
+  imageColors,
+  images,
+} from '../models/images';
 import { authenticate } from '../models/user';
 
 const router = Router();
 
 router.post('/images/download', downloadImage);
+router.post('/images/colors', imageColors);
 router.get('/images/:query/:page/:aspectRatio/:color', images);
 router.get('/images/:imageId', authenticate(), getImage);
 router.post('/images', authenticate(), addImage);
